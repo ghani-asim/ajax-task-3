c19beded524d21cdd344876fc64682331d0c1dad
ghani-asim <44016343+ghani-asim@users.noreply.github.com>
2019-11-13 18:05:37 +0200

    task done

---
+++ js/main-a.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const ul = document.querySelector('ul');
+
+const showImages = async (code) => {
+    ul.innerHTML = await code;
+};
+
+const getFetchData = async (url) => {
+    const response = await fetch(url);
+    const result = await response.text();
+    return result;
+ };
+
+ try {
+    const code = getFetchData('images.html');
+    showImages(code);
+ } catch(e) {
+    console.error(e);
+ }
+ 
+ 
